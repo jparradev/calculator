62e07ba85675f65d1ae0661bb2e435be07ceea84
Jeremy Parra <jparradev@gmail.com>
2019-09-05 09:15:48 -0600

    making live, adding markdown content

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# calculator
-Mock iOS calculator
+# Mock iOS Calculator
+
+This app has been designed to function like the basic iOS calculator. No scientific calculator functions have been added at this time.
+
+![Screenshot](/Media/calc.png)
+
+## Technologies Used
+
+This apps fucntionality is designed in jQuery. Design done in CSS is intended to look like the iOS calculaor app.
+
+### Link to App
+
+[Click Me!](https://jparradev.github.io/calculator/)
+
+
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    // Makes variables global for runtime of app
     var firstNumber = 0;
     var secondNumber = 0;
     var operator = "";
@@ -10,7 +9,6 @@ $(document).ready(function () {
 
 
 
-    // Function to reset app, or "clear". Also initializes upon loading app
     function initializeCalculator() {
         firstNumber = "";
         secondNumber = "";
@@ -21,15 +19,12 @@ $(document).ready(function () {
         $("#first-number, #second-number, #operator, #result").empty();
     }
 
-    //Function to handle click event for numbers
     $(".number").on("click", function () {
 
-        // If app has already calculated
         if (isCalculated) {
             return false;
         }
 
-        // If operator is chosen, we should be writing the secondNumber, otherwise, the firstNumber
         if (isOperatorChosen) {
             secondNumber += $(this).val();
             $("#result").text(secondNumber);
@@ -44,31 +39,24 @@ $(document).ready(function () {
 
     $(".operator").on("click", function () {
 
-        // Check if a first number has already been selected
-        // Or we've already run a calculation, if so we just exit.
         if (!firstNumber || isCalculated) {
             return false;
         }
 
-        // Set isOperatorChosen to true so we start writing to secondNumber
         isOperatorChosen = true;
 
-        // Store off the operator
         operator = $(this).val();
 
-        // Set the HTML of the #operator to the text of what was clicked
         $("#operator").text($(this).text());
 
     });
 
     $(".pos").on("click", function () {
 
-        // Check if we've already run a calculation, if so... we'll just.
         if (isCalculated) {
             return false;
         }
 
-        // If operator is chosen, we should be writing the secondNumber, otherwise, the firstNumber
         if (isOperatorChosen) {
             secondNumber = ~secondNumber + 1
             console.log(secondNumber);
@@ -85,12 +73,10 @@ $(document).ready(function () {
 
     $(".percent").on("click", function () {
 
-        // Check if we've already run a calculation, if so... we'll just.
         if (isCalculated) {
             return false;
         }
 
-        // If operator is chosen, we should be writing the secondNumber, otherwise, the firstNumber
         if (isOperatorChosen) {
             secondNumber = secondNumber * .01;
             console.log(secondNumber);
@@ -107,19 +93,16 @@ $(document).ready(function () {
 
     $(".equal").on("click", function () {
 
-        // If we already clicked equal, don't do the calculation again
+
         if (isCalculated) {
             return false;
         }
 
-        // Set isCalculated to true so that we don't get in a weird UI state by clicking buttons again
         isCalculated = true;
 
-        // Use 'parseFloat' to convert our string representation of numbers into actual integers
         firstNumber = parseFloat(firstNumber);
         secondNumber = parseFloat(secondNumber);
-        // Based on the operator that was chosen.
-        // Then run the operation and set the HTML of the result of that operation
+
         if (operator === "plus") {
             result = firstNumber + secondNumber;
         }
@@ -142,7 +125,6 @@ $(document).ready(function () {
 
     $(".clear").on("click", function () {
 
-        // Call initializeCalculater so we can reset the state of our app
         initializeCalculator();
 
     });
